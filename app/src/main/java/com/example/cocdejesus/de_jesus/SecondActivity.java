fbff2c57e305414753a4d2ddc9a9931a387b332c
cocdejesus <owenndejesus@gmail.com>
2018-12-07 14:48:40 +0800

    yeaa

--- app/src/main/java/com/example/cocdejesus/de_jesus/SecondActivity.java
+++ app/src/main/java/com/example/cocdejesus/de_jesus/SecondActivity.java
@@ -5,9 +5,14 @@ import android.os.Bundle;
 
 public class SecondActivity extends Activity {
 
+    private int []button = new int[] {R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4, R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8, R.id.btn9, R.id.btn10,
+            R.id.btn11, R.id.btn12, R.id.btn13, R.id.btn14, R.id.btn15, R.id.btn16 };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+
     }
 }
