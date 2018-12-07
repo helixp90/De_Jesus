66c5ffe2c3b458d5e42a9977256aef6dfd5b3991
KUYA <owenndejesus@gmail.com>
2018-12-08 02:05:52 +0800

    SO MUCH CODES

--- app/src/main/java/com/example/cocdejesus/de_jesus/SecondActivity.java
+++ app/src/main/java/com/example/cocdejesus/de_jesus/SecondActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,56 +14,71 @@ import java.util.List;
 
 public class SecondActivity extends Activity {
 
-    public int []buttonid = new int[] {R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4, R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8, R.id.btn9, R.id.btn10,
-            R.id.btn11, R.id.btn12, R.id.btn13, R.id.btn14, R.id.btn15, R.id.btn16 };
+    public int []buttonid;
 
-    public List<Button> buttons;
+    public List<Button> buttons = new ArrayList<>();
 
-    public List<String> letters = Arrays.asList("A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F", "G", "G", "H", "H");
+    public List<String> letters = new ArrayList<>();
 
-    public TextView countdown = findViewById(R.id.countdown);
+    public TextView countdowntxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        for (int i = 0; i < buttonid.length; i++) {
+        buttonid = new int[] {R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4, R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8, R.id.btn9, R.id.btn10,
+                R.id.btn11, R.id.btn12, R.id.btn13, R.id.btn14, R.id.btn15, R.id.btn16 };
 
-            Button button = findViewById(buttonid[i]);
-            buttons.add(button);
-        }
+        letters = Arrays.asList("A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F", "G", "G", "H", "H");
 
-        Collections.shuffle(letters);
+        countdowntxt = findViewById(R.id.countdown);
 
-        for (int i = 0; i < letters.size(); i++) {
+        try {
 
-            buttons.get(i).setText(letters.get(i));
+            for (int i = 0; i < buttonid.length; i++) {
 
-        }
-
-        new CountDownTimer(3000, 1000) {
-
-            public void onTick(long untilFinish) {
-
-                for (int i = 0; i < buttons.size(); i++) {
-
-                    buttons.get(i).setEnabled(false);
-                }
-
-                countdown.setText(Long.toString(untilFinish / 1000));
-                
+                Button button = findViewById(buttonid[i]);
+                buttons.add(button);
             }
 
-            public void onFinish() {
+            Collections.shuffle(letters);
 
-                for (int i = 0; i < buttons.size(); i++) {
+            for (int i = 0; i < letters.size(); i++) {
 
-                    buttons.get(i).setText("???");
-                    buttons.get(i).setEnabled(true);
-                }
+                buttons.get(i).setText(letters.get(i));
+
             }
-        }.start();
+
+            new CountDownTimer(3000, 1000) {
+
+                public void onTick(long untilFinish) {
+
+                    for (int i = 0; i < buttons.size(); i++) {
+
+                        buttons.get(i).setEnabled(false);
+                    }
+
+                    countdowntxt.setText(String.valueOf(untilFinish / 1000));
+
+                }
+
+                public void onFinish() {
+
+                    for (int i = 0; i < buttons.size(); i++) {
+
+                        buttons.get(i).setText("???");
+                        buttons.get(i).setEnabled(true);
+                    }
+                }
+            }.start();
+        }
+
+        catch (Exception e) {
+
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
 
     }
 }
